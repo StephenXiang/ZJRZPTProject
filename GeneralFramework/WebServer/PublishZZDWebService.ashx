be1532d3e07d4c332afd9195fff12b0e1ced53b9
StephenXiang <122116961@qq.com>
2017-06-05 21:39:00 +0800

    zzdfb

---
+++ GeneralFramework/WebServer/PublishZZDWebService.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="PublishZZDWebService.ashx.cs" Class="GeneralFramework.WebServer.PublishZZDWebService" %>
---
+++ GeneralFramework/WebServer/PublishZZDWebService.ashx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.SessionState;
+
+namespace GeneralFramework.WebServer
+{
+    /// <summary>
+    /// PublishZZDWebService 的摘要说明
+    /// </summary>
+    public class PublishZZDWebService : IHttpHandler
+    {
+
+        HttpRequest Request;
+        HttpResponse Response;
+        HttpSessionState Session;
+        HttpServerUtility Server;
+        HttpCookie Cookie;
+        HttpContext context;
+        HttpFileCollection files;
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Buffer = true;
+            context.Response.ExpiresAbsolute = DateTime.Now.AddDays(-1);
+            context.Response.AddHeader("pragma", "no-cache");
+            context.Response.AddHeader("cache-control", "");
+            context.Response.CacheControl = "no-cache";
+            context.Response.ContentType = "text/plain";
+
+            files = context.Request.Files;
+            Request = context.Request;
+            Response = context.Response;
+            Session = context.Session;
+            Server = context.Server;
+
+
+            string method = Request["method"].ToString();
+            MethodInfo methodInfo = this.GetType().GetMethod(method);
+            try
+            {
+                methodInfo.Invoke(this, null);
+            }
+            catch (TargetInvocationException targetEx)
+            {
+                if (targetEx.InnerException != null)
+                {
+                    throw targetEx.InnerException;
+                }
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void SaveZZDInfo()
+        {
+            var data = Request;
+            var sr = new StreamReader(data.InputStream);
+            var stream = sr.ReadToEnd();
+        }
+    }
+}
