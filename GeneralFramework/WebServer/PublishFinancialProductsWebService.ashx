b0c4628450f74a7b9c1791095b2f10043c9c3dbf
StephenXiang <122116961@qq.com>
2017-06-05 22:41:04 +0800

    jrcpfb

---
+++ GeneralFramework/WebServer/PublishFinancialProductsWebService.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="PublishFinancialProductsWebService.ashx.cs" Class="GeneralFramework.WebServer.PublishFinancialProductsWebService" %>
---
+++ GeneralFramework/WebServer/PublishFinancialProductsWebService.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeneralFramework.WebServer
+{
+    /// <summary>
+    /// PublishFinancialProductsWebService 的摘要说明
+    /// </summary>
+    public class PublishFinancialProductsWebService : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
