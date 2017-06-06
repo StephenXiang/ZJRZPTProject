79228559722ec275ef4d850299ca92168a5e384b
yinweiwen <yww0828@126.com>
2017-06-07 00:57:08 +0800

    JRCPSave

---
+++ GeneralFrameworkBLLModel/JRCPInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralFrameworkBLLModel
+{
+    public class JRCPInfo
+    {
+        public string UserName;
+        public string jrname;
+        public int dkqxstart;
+        public int dkqxend;
+        public int jrdanbao;
+        public double dkedstart;
+        public double dkedend;
+        public double llfwstart;
+        public double llfwend;
+        public string jrlxdh;
+        public string jrcpjj;
+        public string jrcptd;
+        public string jrsykh;
+        public string jrsqtj;
+        public string jrcailiao;
+        public byte[] logo;
+    }
+}
